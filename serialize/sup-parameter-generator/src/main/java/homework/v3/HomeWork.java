61084fa86c6af38993a3a7f460037ce72ac14dc3
Your Name <you@example.com>
2019-02-26 14:04:22 +0300

    initial

---
+++ serialize/sup-parameter-generator/src/main/java/homework/v3/HomeWork.java
@@ -0,0 +1,19 @@
+package homework.v3;
+
+
+/**
+ * Задание
+ * 1) Составить файл с JSON-объектом, который "разложен" в пакете homework.v3.entity.
+ * Определить какой элемент является корневым
+ * Дать имя файлу homework.parameters.json
+ * 2) Заполнить его значениями (как пример "parameters.v1.json")
+ * 3) Считать получившийся homework.parameters.json в память
+ * 4) Сериализовать его с помощью встроенного механиза сериализации в файл с именем homework.parameters.ser
+ * 5) Сериализовать его с использованием интерфейса Externalizable в файл с именем homework.parameters.exter
+ * 6) Считать данные из файла homework.parameters.ser в память и сохранить в формате JSON в файл с именем homework.result.ser.parameters.json
+ * 7) Считать данные из файла homework.parameters.exter в память и сохранить в формате JSON в файл с именем homework.result.exter.parameters.json
+ * 8) Убедиться, что файлы homework.result.ser.parameters.json и  homework.result.exter.parameters.json
+ * совпадают с homework.parameters.json
+ * */
+public class HomeWork {
+}
